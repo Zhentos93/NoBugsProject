289348f32740c297fc0042d8e39265902e285ec3
Evgenii Kandalov <Zhentos0992@mail.ru>
2026-07-20 21:41:14 +0400

    Homework_Troubleshooting_Fix

--- src/main/java/practice_17_3_homework_Trubleshuting/DebugTask4.java
+++ src/main/java/practice_17_3_homework_Trubleshuting/DebugTask4.java
@@ -4,9 +4,13 @@ package practice_17_3_homework_Trubleshuting;
 
 public class DebugTask4 {
     public static void main(String[] args) {
-        System.out.println(isPalindrome("abba"));
+        System.out.println(isPalindrome(null));
     }
     public static boolean isPalindrome(String str) {
+        if (str == null) {
+            throw new IllegalArgumentException("str cannot be null");
+        }
+
         String reversed = new StringBuilder(str).reverse().toString();
         return str.equals(reversed);
     }
